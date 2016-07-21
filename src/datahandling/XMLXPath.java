0967af7ff9ada94a75e034649e74737403309caf
Rasmus Højte <rasmusvh@msn.com>
2016-07-21 15:26:09 +0200

    Update XMLXPath.java

--- src/datahandling/XMLXPath.java
+++ src/datahandling/XMLXPath.java
@@ -1,8 +1,4 @@
 package xmlxpath;
-/**
- *
- * @author Dora
- */
 
 import java.io.File;
 import java.io.IOException;
@@ -28,34 +24,26 @@ public class XMLXPath
     {
         File myxmlfile = new File("Employees.xml");
         
-        // Path expressions
         String str1 = "//Employee[name='Jarl']/role/text()";
         String str2 = "count(//Employee[role='Manager'])";
 
-        // standard for reading an XML file    
         DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = builderFactory.newDocumentBuilder();
         Document doc = builder.parse(myxmlfile);
 
-        // create an XPathFactory and an XPath object
         XPathFactory xFactory = XPathFactory.newInstance();
         XPath xpath = xFactory.newXPath();
         XPathExpression expr1, expr2; 
 
-        // compile the XPath expression to get the role of Jarl
         expr1 = xpath.compile(str1); 
-        // run the query and get a nodeset as a result
         Object result = expr1.evaluate(doc, XPathConstants.NODESET);
-        // cast the result to a DOM NodeList
         NodeList results = (NodeList) result;
         for (int i=0; i<results.getLength(); i++)
         {
           System.out.println(results.item(i).getNodeValue());
         }
 
-        // new XPath expression to get the number of Managers
         expr2 = xpath.compile(str2);
-        // run the query and get the number of nodes, returned result is Double
         Double number = (Double) expr2.evaluate(doc, XPathConstants.NUMBER);
         System.out.printf("Number of Managers:%2.0f\n", number);
     }
